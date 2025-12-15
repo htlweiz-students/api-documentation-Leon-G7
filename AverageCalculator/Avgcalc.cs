21d874b2fa73f3682f8662ea75ad41fcde08835a
Student <foobar@sr.htlweiz.at>
2025-12-15 10:59:23 +0100

    AverageCalculator

---
+++ AverageCalculator/Avgcalc.cs
@@ -0,0 +1,30 @@
+﻿namespace AverageCalculator;
+
+public class Class1
+{
+    public double add(double value) {
+        werte.add(value);
+    }
+    public double add(double[] value) {
+        foreach (double value in value)
+        {
+            werte.Add(value);
+        }
+    }
+    public double getAverage(double[] value, value.Length) {
+        if (value < null || value.Length < 0){
+            int average = 0;
+            avrage = value / value.Length;
+            return average;
+        } if (value == null || value.Length == 0) {
+            return;
+        }
+    }
+    public double[] getElements() {
+
+    }
+    public int count(value) {
+        numbercount = value.Length;
+        return numbercount;
+    }
+}
